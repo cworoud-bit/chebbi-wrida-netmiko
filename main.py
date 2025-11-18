7227cad1bec52bc39b50e8b79643497746943c5c
wrida <cworoud@gmail.com>
2025-11-18 10:35:11 +0100

    Ajout de la fonctio acces_netmiko

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-# main.py
+"""
+from netmiko import ConnectHandler
+
 def acces_netmiko():
-# Affiche la date coté routeur
-# Affiche les interfaces du routeur dans un fichier interfaces.txt
-print("Hello, Git!")
-dire_bonjour()
+    cisco_router = {
+        "device_type": "cisco_ios",
+        "host": "sandbox-iosxr-1.cisco.com",
+        "username": "admin",
+        "password": "C1sco12345",
+        "port": 22,
+        "secret": "",
+    }
+    net_connect = ConnectHandler(**cisco_router)
+    print(net_connect.send_command("show clock"))
+    interfaces = net_connect.send_command("show ip interface brief")
+    with open("interfaces.txt", "w") as f:
+        f.write(interfaces)
